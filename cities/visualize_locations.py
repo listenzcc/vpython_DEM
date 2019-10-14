9a18b6efea82f9bfce0d1e520a7894779f203a4a
Chuncheng Zhang <listenzcc@mail.bnu.edu.cn>
2019-10-14 23:06:07 +0800

    Modify visualize_locations.
    Speedup and draw legend.

--- cities/visualize_locations.py
+++ cities/visualize_locations.py
@@ -20,33 +20,15 @@ locations['山西省']['万荣'][1] = '37.20'
 
 # pprint(locations)
 
-num_cities = 0
 N_range = [90, 0]
 E_range = [180, 0]
 
-####################################################
-# These code is to make fixed size ax
-fig = plt.figure(figsize=(6, 6))
-# The first items are for padding and the second items are for the axes.
-# sizes are in inch.
-h = [Size.Fixed(0.5), Size.Fixed(5)]
-v = [Size.Fixed(0.5), Size.Fixed(5)]
-divider = Divider(fig, (0.0, 0.0, 1., 1.), h, v, aspect=False)
-# the width and height of the rectangle is ignored.
-ax = Axes(fig, divider.get_position())
-ax.set_axes_locator(divider.new_locator(nx=1, ny=1))
-fig.add_axes(ax)
-
-# For each state
-colors = np.random.rand(100)
+info_states = dict()
 for state_name, cities in locations.items():
     print(state_name)
-    # For each city
-    color = np.random.rand(3).reshape(1, 3)
+    info_states[state_name] = dict(names=[],
+                                   pos=[])
     for name, pos_str in cities.items():
-        num_cities += 1
-        # Formation is name: [E, N]
-        # Compute position
         pos = [float(pos_str[0]), float(pos_str[1])]
         if pos[0] < E_range[0]:
             E_range[0] = pos[0]
@@ -56,16 +38,34 @@ for state_name, cities in locations.items():
             N_range[0] = pos[1]
         if pos[1] > N_range[1]:
             N_range[1] = pos[1]
+        info_states[state_name]['names'].append(name)
+        info_states[state_name]['pos'].append(pos)
 
-        print(name, pos)
-        # Draw a point
-        ax.scatter(pos[0], pos[1], s=5, c=color, label=state_name,
-                   marker='.', alpha=0.5, edgecolor=None)
-        # Mark name
-        # axe.text(pos[0], pos[1], u'%s' % name)
-# ax.legend()
+    info_states[state_name]['pos'] = np.array(info_states[state_name]['pos'])
 
-print(num_cities, N_range, E_range)
+pprint(info_states)
+
+####################################################
+# These code is to make fixed size ax
+fig = plt.figure(figsize=(10, 10))
+# The first items are for padding and the second items are for the axes.
+# sizes are in inch.
+h = [Size.Fixed(0.5), Size.Fixed(9)]
+v = [Size.Fixed(0.5), Size.Fixed(9)]
+divider = Divider(fig, (0.0, 0.0, 1., 1.), h, v, aspect=False)
+# the width and height of the rectangle is ignored.
+ax = Axes(fig, divider.get_position())
+ax.set_axes_locator(divider.new_locator(nx=1, ny=1))
+fig.add_axes(ax)
+
+for state_name, info in info_states.items():
+    print(state_name, len(info['names']))
+    num = len(info['names'])
+    x, y = info['pos'][:, 0], info['pos'][:, 1]
+    colors = np.repeat(np.random.rand(3).reshape(1, 3) * 0.8,
+                       num, axis=0)
+    ax.scatter(x, y, s=5, c=colors, label=state_name)
+ax.legend(loc='best', bbox_to_anchor=(1, 1))
 
 padding = 5
 ax.set_ylim(N_range[0]-padding, N_range[0]+E_range[1]-E_range[0]+padding)
